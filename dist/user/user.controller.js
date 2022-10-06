a842de7856e620486b1c18eae9610db6862bc1d3
YOUR NAME <YOUR EMAIL ADDRESS>
2022-10-06 20:07:58 +0500

    .

--- dist/user/user.controller.js
+++ dist/user/user.controller.js
@@ -218,7 +218,7 @@ let UserController = class UserController {
                     data: data
                 };
             });
-            console.log('deleteUser', deleteUser);
+            return deleteUser;
         }
         else {
             return {
